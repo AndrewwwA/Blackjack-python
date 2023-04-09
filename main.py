a34257c37028287ade418ef7ffdae432dd0d6057
Andrew A <pokemonnibbler22@gmail.com>
2023-04-09 01:10:14 -0500

    new class that creates a Hand for dealer + player

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import random
 
+        
+# Class to create a new card
+
+class Card:
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+    def __str__(self):
+        return f'{self.rank["rank"]} of {self.suit}'
+
+# Class to create a new deck of cards 
+
 class Deck: 
     def __init__(self):
         self.cards = []
@@ -8,19 +20,68 @@ class Deck:
 
         for suit in suits:
             for rank in ranks:
-                self.cards.append([suit, rank])
+                self.cards.append(Card(suit, rank))
             
     def shuffleCards(self): 
         if (len(self.cards) != 0):
             random.shuffle(self.cards)   
-    def dealCard(self, num):
+    def dealCards(self, num):
             deltCards = []
             for i in range(num):
                 if len(self.cards) != 0: 
                     deltCards.append(self.cards.pop())
-            return deltCards
+            return deltCards   
         
-    # Testing purposes = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = 
+# Class to create a hand of cards
+
+class Hand:
+    def __init__(self, house = False):
+        self.cards = []
+        self.value = 0
+        self.house = house
+    def addCard(self, cardsList):
+        self.cards.extend(cardsList)
+    def calculateHand(self):
+        self.value = 0
+        Ace = False
+        for card in self.cards:
+            if card == 'A':
+                Ace = True
+            cardValue = int(card.rank["value"])
+            self.value += cardValue
+        if Ace and self.value > 21:
+            self.value -= 10
+    def getValue(self):
+        self.calculateHand()
+        return self.value
+    def isBlackJack(self):
+        if self.getValue() == 21: return True
+        return False
+    def showHand(self, showDealer = False):
+        if self.house == True: 
+            print(f'Dealer\'s Hand: ')
+            for index, card in enumerate(self.cards):
+                if index == 0 and not showDealer and not self.isBlackJack(): print()
+                else: print(card)
+            print(f'Value: {self.getValue()}')
+            print()
+        else: 
+            print(f'Player\'s Hand: ')
+            for card in self.cards:
+                print(card)
+            print(f'Value: {self.getValue()}')
+            print()
+
+# deck = Deck()
+# deck.shuffleCards()
+
+# hand = Hand()
+# hand.addCard(deck.dealCards(2))
+# hand.showHand()
+
+# Testing purposes = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = 
+# newCard = Card('hearts', {"rank": 'A', "value": 11})
+# print(newCard)
 # newdeck = Deck()
 # newdeck.shuffleCards()
 # print(newdeck.cards)
