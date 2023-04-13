0981d34ef2e3528aa2366073f59a03bb93d392fd
Andrew A <andrewjacobalvarez22@gmail.com>
2023-04-13 12:22:19 -0500

    testing area

--- testing.py
+++ testing.py
@@ -1,20 +1,27 @@
 import tkinter as tk
-from tkinter import ttk
+import math
+
+root = tk.Tk()
+root.title("Flexbox-like container in Tkinter")
+
+def create_flex_container(parent, widget_count):
+    parent.grid_rowconfigure(0, weight=1)
+    for column in range(widget_count):
+        parent.grid_columnconfigure(column, weight=1)
+
+    return widget_count
 
 
-window = tk.Tk()
-window.title('Getting and setting')
-window.geometry('600x500')
+widget_count = 8  # Adjust this value to change the number of cards
+container = tk.Frame(root)
+columns = create_flex_container(container, widget_count)
+container.pack(fill=tk.BOTH, expand=True)
 
-text = tk.Text(window)
-text.pack()
+for i in range(widget_count):
+    button = tk.Button(container, text=f"Button {i + 1}")
+    button.grid(row=0, column=i, sticky=tk.NSEW)
 
-entry = ttk.Entry(window)
-entry.pack()
 
-btn = ttk.Button(window, text='Click me')
-btn.pack()
 
-text.bind('<Shift-MouseWheel>', lambda event: print('mousewheel'))
 
-window.mainloop()
+root.mainloop()
